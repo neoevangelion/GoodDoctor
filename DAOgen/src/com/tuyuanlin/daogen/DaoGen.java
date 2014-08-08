0c87eb7b745d6868a4187570aecab5fb0ff85e41
neoevangelion <yuanlin.tu@gmail.com>
2014-08-08 16:18:42 +0800

    add DAO module

---
+++ DAOgen/src/com/tuyuanlin/daogen/DaoGen.java
@@ -0,0 +1,35 @@
+package com.tuyuanlin.daogen;
+
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.DaoGenerator;
+
+/**
+ * Created by eWalk_iOS on 14-8-8.
+ */
+public class DaoGen {
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1, "com.gooddoctor.data");
+        Entity disease = schema.addEntity("Disease");
+
+        disease.addIdProperty();
+        disease.addStringProperty("name");
+        disease.addStringProperty("pinyin");
+        disease.addIntProperty("productNum");
+
+        Entity medicine = schema.addEntity("Medicine");
+
+        medicine.addIdProperty();
+        medicine.addStringProperty("name");
+        medicine.addStringProperty("commonName");
+        medicine.addStringProperty("efficacy");
+        medicine.addStringProperty("imageUrl");
+        medicine.addStringProperty("companyName");
+        medicine.addLongProperty("price");
+        medicine.addIntProperty("type");
+        medicine.addIntProperty("attribute");
+
+        new DaoGenerator().generateAll(schema, "../app/src/main/java/");
+
+    }
+}
